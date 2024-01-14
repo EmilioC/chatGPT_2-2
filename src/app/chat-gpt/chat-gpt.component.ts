18cf8f78313796697d01a5c9aaad6ca8c89b49af
EmilioC <emicorgul@gmail.com>
2024-01-14 23:58:11 +0100

    Fisrt commmit

---
+++ src/app/chat-gpt/chat-gpt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-gpt',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-gpt.component.html',
+  styleUrl: './chat-gpt.component.css'
+})
+export class ChatGPTComponent {
+
+}
