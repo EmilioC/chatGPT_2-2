8edac22a0314487667d23af71d05cde5f851b941
EmilioC <emicorgul@gmail.com>
2024-01-17 01:20:51 +0100

    Update sevices

--- src/app/chat-gpt/chat-gpt.component.ts
+++ src/app/chat-gpt/chat-gpt.component.ts
@@ -2,41 +2,48 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatgptService } from '../services/chatgpt.service';
 import { FormsModule } from '@angular/forms';
+import { ChatWithBot } from '../models/gpt-response';
 
 
 @Component({
   selector: 'app-chat-gpt',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat-gpt.component.html',
   styleUrl: './chat-gpt.component.css'
 })
 export class ChatGPTComponent {
 
-  promptText = '¿Por qué llueve arroz los martes?';
+  promptText = '¿Por qué llueve caramelos?';
+  chatConversation: ChatWithBot[] = [];
+  showSpinner = false;
 
   constructor(private chatgptSvc: ChatgptService) { }
 
-  checkResponse() {
-    console.log("PROMPT: " + this.promptText);
-    console.log("START -- checkResponse");
-    this.chatgptSvc.getResponse(this.promptText);
-    console.log("END -- checkResponse");
+  async checkResponse() {
+    try {
+      this.showSpinner = true
+      const response = await this.chatgptSvc.getResponse(this.promptText);
+      if (response) {
+        const chat = this.chatgptSvc.pushChatContent(response, 'BOT', 'USER');
+        console.log("CHAT", chat);
+        this.chatConversation = chat;
+        this.showSpinner = false// Cambia 'Fistro pecador' y 'person' según sea necesario
+      }
+      // this.promptText = '';
+    } catch (error) {
+      console.error('Error al obtener la respuesta del servicio:', error);
+    }
   }
 
 
-
-
-  /*   sendMessage() {
-
-      this.chatgptSvc.getDataFromOpenAI(this.message);
-      this.message = '';
-    } */
-
-  limpiar() {
-
-    location.reload();
-
+  getText(data: string) {
+    // Verifica si 'data' está definido y no es null antes de llamar a 'split'
+    if (data) {
+      return data.split('\n').filter(f => f.length > 0);
+    } else {
+      // Retorna un valor predeterminado o un array vacío si 'data' es undefined o null
+      return [];
+    }
   }
-
 }
--- src/app/services/chatgpt.service.ts
+++ src/app/services/chatgpt.service.ts
@@ -1,34 +1,61 @@
 import { Injectable } from '@angular/core';
 import OpenAI from 'openai'; // Asegúrate de que el SDK de OpenAI esté instalado y disponible
 import { environment } from '../environments/environment';
-import { ChatWithBot, ResponseModel } from '../models/gpt-response';
+import { ChatWithBot, ResponseModel, ResponseModelTurbo, message } from '../models/gpt-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatgptService {
   private openai: OpenAI;
-  response!: ResponseModel;
+  response!: any;
   chatConversation: ChatWithBot[] = [];
   messages: string[] = [];
+  promptText: string = '';
 
   constructor() {
     // Inicializa el cliente de OpenAI con la API Key desde el archivo de entorno
     this.openai = new OpenAI({ apiKey: environment.apiKey, dangerouslyAllowBrowser: true });
   }
 
+  pushChatContent(content: string, person: string, cssClass: string) {
+    const chatToPush: ChatWithBot = { person: person, response: content, cssClass: cssClass };
+    console.log("CHAT TO PUSH", chatToPush);
+    this.chatConversation.unshift(chatToPush);
+    return this.chatConversation
+
+  }
+
+  checkResponse(promptText: string) {
+    this.pushChatContent(promptText, 'Fistro pecador', 'person');
+    this.getResponse(promptText);
+    return this.pushChatContent;
+
+  }
+
   // Método para obtener una respuesta de GPT-3.5
-  async getResponse(message: string): Promise<any> {
+  async getResponse(message: string) {
     try {
-      const response = await this.openai.chat.completions.create({
+      const apiResponse = await this.openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [{ role: "system", content: "Eres un cómico español satírico del siglo 18" },
         { role: "user", content: message }]
         , temperature: 1,
         max_tokens: 100
       });
-      console.log("RESPONSE: " + response.choices[0].message.content);
-      return response.choices[0].message.content; // Retorna solo el contenido del mensaje de respuesta
+      //console.log("RESPONSE: " + response.choices[0].message.content);
+      console.log("RESPONSE BRUTO: ", apiResponse);
+
+      this.response = apiResponse.choices;
+
+      /* this.responseX = response.choices as unknown as ResponseModel; */
+
+
+      this.pushChatContent(this.response[0].message.content, 'Chiquitron', 'bot');
+
+
+      return apiResponse.choices[0].message.content; // Retorna solo el contenido del mensaje de respuesta
+
     } catch (error) {
       console.error('Error al solicitar a OpenAI:', error);
       throw error; // O maneja el error según lo requiera tu aplicación
